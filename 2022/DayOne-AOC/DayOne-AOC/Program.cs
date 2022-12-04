5f510bafd0e02603b71fea9f99cac97dd20b67dd
Yohko <elisha5208@gmail.com>
2022-12-04 17:45:31 -0500

    feat(back) : implemented function to calculate which elf carries the most calories

--- 2022/DayOne-AOC/DayOne-AOC/Program.cs
+++ 2022/DayOne-AOC/DayOne-AOC/Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Runtime.CompilerServices;
+
+var entryValues = ReadEntryValuesFromFile();
+var result = CalculateMaxcaloriesCarriedByElf(entryValues);
+Console.WriteLine($"The most amount of calories ({result.amount}) is carried by elf {result.elf+1}");
+
+Dictionary<int, List<int>> ReadEntryValuesFromFile()
+{
+    var directory = AppDomain.CurrentDomain.BaseDirectory;
+    var result = new Dictionary<int, List<int>>();
+    using (var reader = new StreamReader($@"{directory}\data.csv"))
+    {
+        List<int> calorieListOfAnElf = new List<int>();
+        int i = 0;
+        while (!reader.EndOfStream)
+        {
+            var value = reader.ReadLine();
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                result.Add(i, calorieListOfAnElf);
+                calorieListOfAnElf = new List<int>();
+                i++;
+            }
+            else
+            {
+                calorieListOfAnElf.Add(Int32.Parse(value));
+            }
+        }
+    }
+    return result;
+}
+
+(int amount, int elf) CalculateMaxcaloriesCarriedByElf(Dictionary <int, List<int>> calorieList)
+{
+    int maxCalorie = 0;
+    int elf = 0;
+    foreach(var elfCalorie in calorieList)
+    {
+        var sum = elfCalorie.Value.Sum();
+        if (sum > maxCalorie)
+        {
+            maxCalorie = sum;
+            elf = elfCalorie.Key;
+        }
+    }
+    result.elf = elf;
+    result.amount = maxCalorie;
+    return result;
+}
